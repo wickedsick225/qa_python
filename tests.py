35375c07900301d7828f638c0ae56844b5ff2cf2
Egor Kravchenko <wickedsick225@yandex.ru>
2025-08-03 15:54:06 +0300

    finally

--- tests.py
+++ tests.py
@@ -10,54 +10,46 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert len(collector.get_books_genre()) == 2
 
-
-
     @pytest.mark.parametrize('name', ['', 'a'*41])
     def test_add_new_book_invalid_name(self, collector, name):
         collector.add_new_book(name)
         assert name not in collector.books_genre
 
-
-    def test_set_book_genre_success(self,collector):
+    def test_set_book_genre_success(self, collector):
         collector.add_new_book('Книга')
-        collector.set_book_genre('Книга','Фантастика')
+        collector.set_book_genre('Книга', 'Фантастика')
         assert collector.get_book_genre('Книга') == 'Фантастика'
 
+    def test_set_book_genre_nonexistent_book(self, collector):
+        collector.set_book_genre('Несуществующая книга', 'Фантастика')
+        assert collector.get_book_genre('Несуществующая книга') is None
 
+    def test_get_books_with_specific_genre(self, collector):
+        collector.add_new_book('Фантастическая книга')
+        collector.set_book_genre('Фантастическая книга', 'Фантастика')
+        assert 'Фантастическая книга' in collector.get_books_with_specific_genre('Фантастика')
 
-    def test_get_books_for_children(self, collector):
+    def test_get_child_friendly_books_included(self, collector):
+        collector.add_new_book('Детская книга')
+        collector.set_book_genre('Детская книга', 'Мультфильмы')
+        assert 'Детская книга' in collector.get_books_for_children()
+
+    def test_get_child_friendly_books_excluded(self, collector):
         collector.add_new_book('Недетская книга')
-        collector.set_book_genre('Недетская книга','Ужасы')
+        collector.set_book_genre('Недетская книга', 'Ужасы')
         assert 'Недетская книга' not in collector.get_books_for_children()
-
 
     def test_add_book_in_favorites(self, collector):
         collector.add_new_book('Любимая книга')
         collector.add_book_in_favorites('Любимая книга')
         assert 'Любимая книга' in collector.get_list_of_favorites_books()
 
-
     def test_add_book_in_favorites_nonexistent_book(self, collector):
         collector.add_book_in_favorites('Жеребенок')
         assert 'Жеребенок' not in collector.get_list_of_favorites_books()
-
 
     def test_delete_book_from_favorites(self, collector):
         collector.add_new_book('Исчезнувшая')
         collector.add_book_in_favorites('Исчезнувшая')
         collector.delete_book_from_favorites('Исчезнувшая')
         assert 'Исчезнувшая' not in collector.get_list_of_favorites_books()
-    def test_set_book_genre_nonexistent_book(self,collector):
-        assert 'Детская книга' in collector.get_books_for_children()
-        collector.set_book_genre('Несуществующая книга','Фантастика')
-        assert collector.get_book_genre('Несуществующая книга') is None
-        collector.set_book_genre('Детская книга','Мультфильмы')
-
-
-        collector.add_new_book('Детская книга')
-    def test_get_books_with_specific_genre(self, collector):
-        collector.add_new_book('Фантастическая книга')
-    def test_get_books_for_children(self, collector):
-        collector.set_book_genre('Фантастическая книга', 'Фантастика')
-        assert 'Фантастическая книга' in collector.get_books_with_specific_genre('Фантастика')
-
